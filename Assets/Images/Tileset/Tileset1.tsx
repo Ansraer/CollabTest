84079f0e987548cc8466f5b785dc34a79a8b9dfe
Jacky2611 <jacky2611@gmail.com>
2017-07-02 19:19:10 +0200

    Added first test map

---
+++ Assets/Images/Tileset/Tileset1.tsx
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Taragonia-BW-MIX / Ultimate Mix Edition 2" tilewidth="16" tileheight="16" tilecount="6106" columns="86">
+ <image source="Taragonia-BW-MIX  Ultimate Mix Edition 2.png" width="1376" height="1136"/>
+ <terraintypes>
+  <terrain name="Gras" tile="178"/>
+  <terrain name="Desert" tile="99"/>
+  <terrain name="Rocks" tile="529"/>
+  <terrain name="Island" tile="3904"/>
+  <terrain name="Ocean" tile="4497"/>
+  <terrain name="Mud" tile="742"/>
+ </terraintypes>
+ <tile id="12" terrain=",,,1"/>
+ <tile id="13" terrain=",,1,1"/>
+ <tile id="14" terrain=",,1,"/>
+ <tile id="86" terrain="0,0,0,"/>
+ <tile id="87" terrain="0,0,,0"/>
+ <tile id="98" terrain=",1,,1"/>
+ <tile id="99" terrain="1,1,1,1"/>
+ <tile id="100" terrain="1,,1,"/>
+ <tile id="172" terrain="0,,0,0"/>
+ <tile id="173" terrain=",0,0,0"/>
+ <tile id="174" terrain=",,,0"/>
+ <tile id="175" terrain=",,0,"/>
+ <tile id="176" terrain=",0,,0"/>
+ <tile id="177" terrain="0,,0,"/>
+ <tile id="178" terrain="0,0,0,0"/>
+ <tile id="184" terrain=",1,,"/>
+ <tile id="185" terrain="1,1,,"/>
+ <tile id="186" terrain="1,,,"/>
+ <tile id="260" terrain=",0,,"/>
+ <tile id="261" terrain="0,,,"/>
+ <tile id="262" terrain=",,0,0"/>
+ <tile id="263" terrain="0,0,,"/>
+ <tile id="270" terrain=",1,1,1"/>
+ <tile id="271" terrain="1,,1,1"/>
+ <tile id="278">
+  <objectgroup draworder="index">
+   <object id="1" x="9.0625" y="-2.5625" width="6.875" height="11.4375"/>
+  </objectgroup>
+ </tile>
+ <tile id="279">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0625" y="-0.1875" width="6.8125" height="9.0625"/>
+  </objectgroup>
+ </tile>
+ <tile id="356" terrain="1,1,,1"/>
+ <tile id="357" terrain="1,1,1,"/>
+ <tile id="442" terrain=",,,2"/>
+ <tile id="443" terrain=",,2,2"/>
+ <tile id="444" terrain=",,2,"/>
+ <tile id="528" terrain=",2,,2"/>
+ <tile id="529" terrain="2,2,2,2"/>
+ <tile id="530" terrain="2,,2,"/>
+ <tile id="602">
+  <objectgroup draworder="index">
+   <object id="6" x="1.03125" y="0.96875" width="10.0625" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="603">
+  <objectgroup draworder="index">
+   <object id="1" x="5.0625" y="0" width="9.9375" height="14.9688"/>
+  </objectgroup>
+ </tile>
+ <tile id="604">
+  <objectgroup draworder="index">
+   <object id="1" x="5.03125" y="1.03125" width="9.96875" height="14.9375"/>
+  </objectgroup>
+ </tile>
+ <tile id="614" terrain=",2,,"/>
+ <tile id="615" terrain="2,2,,"/>
+ <tile id="616" terrain="2,,,"/>
+ <tile id="655" terrain=",,,5"/>
+ <tile id="656" terrain=",,5,5"/>
+ <tile id="657" terrain=",,5,"/>
+ <tile id="688">
+  <objectgroup draworder="index">
+   <object id="1" x="1.0625" y="0.03125" width="10.875" height="15.9375"/>
+  </objectgroup>
+ </tile>
+ <tile id="689">
+  <objectgroup draworder="index">
+   <object id="1" x="1.03125" y="-0.03125" width="9.96875" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="690">
+  <objectgroup draworder="index">
+   <object id="1" x="4.0625" y="0.03125" width="10.9063" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="701" terrain=",2,2,2"/>
+ <tile id="702" terrain="2,,2,2"/>
+ <tile id="708">
+  <objectgroup draworder="index">
+   <object id="1" x="8.9375" y="-0.5625" width="7.0625" height="6.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="709">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0625" y="0" width="6.9375" height="5.9375"/>
+  </objectgroup>
+ </tile>
+ <tile id="741" terrain=",5,,5"/>
+ <tile id="742" terrain="5,5,5,5"/>
+ <tile id="743" terrain="5,,5,"/>
+ <tile id="774">
+  <objectgroup draworder="index">
+   <object id="1" x="4" y="5" width="11.9688" height="9"/>
+   <object id="2" x="1.03125" y="0" width="10.9375" height="11.9688"/>
+  </objectgroup>
+ </tile>
+ <tile id="775">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="5" width="16" height="9.96875"/>
+  </objectgroup>
+ </tile>
+ <tile id="776">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="5.03125" width="13.9688" height="8.9375"/>
+   <object id="2" x="4.03125" y="0" width="11" height="8.9375"/>
+  </objectgroup>
+ </tile>
+ <tile id="787" terrain="2,2,,2"/>
+ <tile id="788" terrain="2,2,2,"/>
+ <tile id="827" terrain=",5,,"/>
+ <tile id="828" terrain="5,5,,"/>
+ <tile id="829" terrain="5,,,"/>
+ <tile id="913" terrain="5,5,5,"/>
+ <tile id="914" terrain="5,5,,5"/>
+ <tile id="999" terrain="5,,5,5"/>
+ <tile id="1000" terrain=",5,5,5"/>
+ <tile id="3817" terrain=",,,3"/>
+ <tile id="3818" terrain=",,3,3"/>
+ <tile id="3819" terrain=",,3,"/>
+ <tile id="3899" terrain="3,3,3,"/>
+ <tile id="3900" terrain="3,3,,3"/>
+ <tile id="3903" terrain=",3,,3"/>
+ <tile id="3904" terrain="3,3,3,3"/>
+ <tile id="3905" terrain="3,,3,"/>
+ <tile id="3985" terrain="3,,3,3"/>
+ <tile id="3986" terrain=",3,3,3"/>
+ <tile id="3989" terrain=",3,,"/>
+ <tile id="3990" terrain="3,3,,"/>
+ <tile id="3991" terrain="3,,,"/>
+</tileset>
