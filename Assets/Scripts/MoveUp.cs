71fd9c6a31029e9a6501e873a20a77775cf4804e
Ansraer <ansraer@gmail.com>
2017-07-02 16:05:34 +0200

    Initial Commit test

---
+++ Assets/Scripts/MoveUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveUp = .1f;
+
+        if (this.transform.position.y > 10)
+            moveUp = -.1f;
+        
+
+        this.transform.Translate(new Vector3(0, moveUp, 0));
+
+
+    }
+}
